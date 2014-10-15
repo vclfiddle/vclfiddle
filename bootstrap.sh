74f0b89da583219c44f0e71ade61f9a7c56884c1
Jason Stangroome <jason@squixa.com>
2014-10-15 14:22:28 +1100

    Install node and sails

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,10 @@
 
 # install docker
 curl -sSL https://get.docker.io/ubuntu/ | sudo sh
+
+# install nodejs
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install --assume-yes nodejs
+
+# install sails.js
+npm install --global sails@0.10.5
