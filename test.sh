b79d1812e33fa9be0aeb3fb8dc0b7939671bef00
Jason Stangroome <jason@squixa.com>
2014-10-16 12:06:29 +1100

    Process request files with netcat. Add debug logging

--- test.sh
+++ test.sh
@@ -7,10 +7,10 @@ sudo docker build --force-rm --tag="varnish4" $REPO_ROOT/varnish4/
 TEST_DIR=/tmp/test-$RANDOM
 mkdir --parents $TEST_DIR
 
-#echo -n "www.google.com" >$TEST_DIR/backend
+echo -e "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n" >$TEST_DIR/request_0
 cp $REPO_ROOT/v4-test.vcl $TEST_DIR/default.vcl
 
-docker run --rm --volume=$TEST_DIR:/fiddle varnish4
+sudo docker run --rm --volume=$TEST_DIR:/fiddle varnish4
 
 ls -alF $TEST_DIR
 # TODO rm $TEST_DIR
