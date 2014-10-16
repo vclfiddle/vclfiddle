bb1162afe6d0bcb1e1c71b20b185912e244e3e84
Jason Stangroome <jason@squixa.com>
2014-10-17 08:59:58 +1100

    Remove sleep calls and SIGKILLs

--- varnish4/run.sh
+++ varnish4/run.sh
@@ -12,13 +12,9 @@ debuglog "Starting varnishd"
 /usr/sbin/varnishd -a 127.0.0.1:80 -f /fiddle/default.vcl -P /run/varnishd.pid 2>&1 >>/fiddle/run.log || exit $?
 debuglog "Started varnishd"
 
-# TODO allow warmup. detect ready.
-sleep 2
-
 debuglog "Starting varnishlog"
 varnishlog -D -v -w /fiddle/varnishlog -P /run/varnishlog.pid 2>&1 >>/fiddle/run.log || exit $?
 debuglog "Started varnishlog"
-sleep 2
 
 debuglog "Executing requests"
 find /fiddle -name request_* -maxdepth 1 -exec /bin/sh -c 'cat {}  | nc 127.0.0.1 80' \; >/dev/null
@@ -27,10 +23,5 @@ debuglog "Executed requests"
 
 debuglog "Flushing varnishlog"
 kill -s SIGUSR1 $(cat /run/varnishlog.pid)
-sleep 2 # TODO find a better way to wait for flush
-
-debuglog "Killing varnishlog and varnishd"
-kill -s SIGKILL $(cat /run/varnishlog.pid)
-kill -s SIGKILL $(cat /run/varnishd.pid)
 
 debuglog "Done"
