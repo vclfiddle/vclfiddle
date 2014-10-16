366e3660e2af05e4614c62f14c7748bbc4f6de71
Jason Stangroome <jason@squixa.com>
2014-10-17 10:52:35 +1100

    Start varnishd without VCL, then compile once and switch

--- varnish4/run.sh
+++ varnish4/run.sh
@@ -4,13 +4,18 @@ function debuglog {
   echo "$(date '+%Y-%m-%d %T.%3N') $@" >>//fiddle/debug.log
 }
 
-debuglog Compiling VCL
-/usr/sbin/varnishd -C -f /fiddle/default.vcl >/fiddle/vcl.c 2>>/fiddle/run.log || exit $?
-debuglog "Compiled VCL"
+function varnishcommand {
+  debuglog "Executing Varnish command: $@"
+  /usr/bin/varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret $@ 2>>/fiddle/run.log || exit $?
+  debuglog "Executed Varnish command: $@"
+}
 
 debuglog "Starting varnishd"
-/usr/sbin/varnishd -a 127.0.0.1:80 -f /fiddle/default.vcl -P /run/varnishd.pid 2>&1 >>/fiddle/run.log || exit $?
+/usr/sbin/varnishd -a 127.0.0.1:80 -b 127.0.0.1:8080 -T 127.0.0.1:6082 -S /etc/varnish/secret -P /run/varnishd.pid 2>&1 >>/fiddle/run.log || exit $?
 debuglog "Started varnishd"
+
+varnishcommand vcl.load fiddle /fiddle/default.vcl
+varnishcommand vcl.use fiddle
 
 debuglog "Starting varnishlog"
 varnishlog -D -v -w /fiddle/varnishlog -P /run/varnishlog.pid 2>&1 >>/fiddle/run.log || exit $?
@@ -18,7 +23,6 @@ debuglog "Started varnishlog"
 
 debuglog "Executing requests"
 find /fiddle -name request_* -maxdepth 1 -exec /bin/sh -c 'cat {}  | nc 127.0.0.1 80' \; >/dev/null
-# TODO replay requests HAR twice here
 debuglog "Executed requests"
 
 debuglog "Flushing varnishlog"
