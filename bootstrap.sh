6c8933efa90bbc6a04b73c5b23cce8ccf5605916
Jason Stangroome <jason@squixa.com>
2014-10-17 13:09:27 +1100

    Replace /vagrant/ path with script root in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+pushd $(dirname $0) >/dev/null || exit $?
+SCRIPTROOT=$(pwd)
+popd >/dev/null
+
 # install docker
 curl -sSL https://get.docker.io/ubuntu/ | sudo sh
 
@@ -8,19 +12,19 @@ curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get install --assume-yes nodejs
 
 # install sails.js
-npm install --global sails@0.10.5
+sudo npm install --global sails@0.10.5
 
 # install the web app
-rsync -av /vagrant/web/ /var/web/
-pushd /var/web && npm install && popd
+sudo rsync -av $SCRIPTROOT/web/ /var/web/
+pushd /var/web && sudo npm install && popd
 
 # build the Varnish docker image
-sudo docker build --force-rm --tag="varnish4" /vagrant/varnish4/
+sudo docker build --force-rm --tag="varnish4" $SCRIPTROOT/varnish4/
 
 # install the setuid run-varnish-container script
 sudo mkdir --parents /opt/vclfiddle/
-sudo gcc /vagrant/run-varnish-container.c -o /opt/vclfiddle/run-varnish-container
-sudo cp /vagrant/run-varnish-container.pl /opt/vclfiddle/run-varnish-container.pl
+sudo gcc $SCRIPTROOT/run-varnish-container.c -o /opt/vclfiddle/run-varnish-container
+sudo cp $SCRIPTROOT/run-varnish-container.pl /opt/vclfiddle/run-varnish-container.pl
 sudo chown root:root /opt/vclfiddle/run-varnish-container*
 sudo chmod 04755 /opt/vclfiddle/run-varnish-container
 sudo chmod 755 /opt/vclfiddle/run-varnish-container.pl
