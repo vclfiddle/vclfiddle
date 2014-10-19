ea8b594a5c04b5faec9f6abb92f3ebca5f44f6e4
Jason Stangroome <jason@squixa.com>
2014-10-20 10:28:51 +1100

    Put entryIndex and summary properties on all requests - included and excluded.

--- web/api/services/RequestMetadataService.js
+++ web/api/services/RequestMetadataService.js
@@ -9,26 +9,42 @@ function convertHarEntriesToRequests (harObject) {
   harObject.log.entries.forEach(function (entry, index) {
     var parsedUrl = url.parse(entry.request.url);
     if (firstHost == null) firstHost = parsedUrl.host;
+    var req = {
+      entryIndex: index,
+      summary: {
+        method: entry.request.method,
+        url: entry.request.url,
+        httpVersion: entry.request.httpVersion
+      }
+    };
     if (parsedUrl.host != firstHost) {
-      excludedRequests.push({entryIndex: index, reason: 'Ignored', message: 'URL Host does not match first request: ' + parsedUrl.host});
+      req.excludeReason = 'Ignored';
+      req.message = 'URL Host does not match first request: ' + parsedUrl.host;
+      excludedRequests.push(req);
     } else if (parsedUrl.protocol != 'http:') {
-      excludedRequests.push({entryIndex: index, reason: 'Unsupported', message: 'Protocol not supported: ' + parsedUrl.protocol});
+      req.excludeReason = 'Unsupported';
+      req.message = 'Protocol not supported: ' + parsedUrl.protocol;
+      excludedRequests.push(req);
     } else if (entry.request.httpVersion != 'HTTP/1.0' && entry.request.httpVersion != 'HTTP/1.1') {
-      excludedRequests.push({entryIndex: index, reason: 'Unsupported', message: 'HTTP version not supported: ' + entry.request.httpVersion});
+      req.excludeReason = 'Unsupported';
+      req.message = 'HTTP version not supported: ' + entry.request.httpVersion;
+      excludedRequests.push(req);
     } else if (entry.request.method != 'GET') {
-      excludedRequests.push({entryIndex: index, reason: 'Unsupported', message: 'Method not supported: ' + entry.request.method});
+      req.excludeReason = 'Unsupported';
+      req.message = 'Method not supported: ' + entry.request.method;
+      excludedRequests.push(req);
     } else {
-      var warnings = [];
-      var payload = entry.request.method + ' ' + parsedUrl.path + ' ' + entry.request.httpVersion + '\r\n';
+      req.warnings = [];
+      req.payload = entry.request.method + ' ' + parsedUrl.path + ' ' + entry.request.httpVersion + '\r\n';
       entry.request.headers.forEach(function (header) {
         if (header.name.toLowerCase() == 'connection') {
-          warnings.push('Connection request header not supported.');
+          req.warnings.push('Connection request header not supported.');
         } else {
-          payload += header.name + ': ' + header.value + '\r\n';
+          req.payload += header.name + ': ' + header.value + '\r\n';
         }
       });
-      payload += '\r\n';
-      requests.push({entryIndex: index, payload: payload, warnings: warnings});
+      req.payload += '\r\n';
+      requests.push(req);
     }
   });
 
