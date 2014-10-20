95dfd5dedad261259778693ac623d913efac2e0a
Jason Stangroome <jason@squixa.com>
2014-10-20 14:47:04 +1100

    Test to expect error when HAR is empty.

--- web/test/unit/controllers/VclController.test.js
+++ web/test/unit/controllers/VclController.test.js
@@ -1,11 +1,20 @@
-var supertest = require('supertest');
+var expect = require('chai').expect;
+var request = require('supertest');
 
 describe('VclController', function () {
 
   describe('#run()', function () {
     it('should require the HAR field to be populated', function (done) {
-      done();
+      request(sails.hooks.http.app)
+        .post('/vcl/run')
+        .set('Accept', 'application/json')
+        .send({ vcl: 'vcl 4.0;', har: '' })
+        .expect('Content-Type', /json/)
+        .end(function (err, res) {
+          if (err) return done(err);
+          expect(res.body.log).to.contain('Failed to parse HAR.');
+          done();
+        });
     });
   });
-
 });
