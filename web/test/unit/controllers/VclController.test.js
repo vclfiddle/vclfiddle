b2b007e167c68a951a3994338da51316ebcdfb88
Jason Stangroome <jason@squixa.com>
2014-10-20 11:29:16 +1100

    Add initial mocha test placeholders

---
+++ web/test/unit/controllers/VclController.test.js
@@ -0,0 +1,11 @@
+var supertest = require('supertest');
+
+describe('VclController', function () {
+
+  describe('#run()', function () {
+    it('should require the HAR field to be populated', function (done) {
+      done();
+    });
+  });
+
+});
