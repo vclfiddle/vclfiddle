a479a4c9b875864ab585fd885fc3852bd2cb2f57
Jason Stangroome <jason@squixa.com>
2014-10-22 14:00:03 +1100

    Implement a local-file-system-based fiddle persistence service

---
+++ web/test/unit/services/FiddlePersistenceService.test.js
@@ -0,0 +1,38 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+
+describe('FiddlePersistenceService', function () {
+
+  describe('#prepareFiddle()', function () {
+
+    it('should return a new fiddle when the input fiddle is undefined', function (done) {
+      var req_body = {};
+      FiddlePersistenceService.prepareFiddle(req_body.id, function (err, fiddle) {
+        if (err) return done(err);
+        expect(fiddle.id).to.match(/^\d{6}-[a-z\d]{7}$/);
+        expect(fiddle.path).to.match(/^\/tmp\/vclfiddle-/);
+        expect(fiddle.path).to.match(/\/\d+\/?$/);
+        fs.stat(fiddle.path, function (err, stats) {
+          if (err) return done(err);
+          expect(stats.isDirectory()).to.be.true;
+          rimraf(path.join(fiddle.path, '..'), done);
+        });
+      });
+    });
+
+    it('should return a new subdir the input fiddle exists', function (done) {
+      var req_body = {};
+      FiddlePersistenceService.prepareFiddle(req_body.id, function (err, fiddleA) {
+        if (err) return done(err);
+        FiddlePersistenceService.prepareFiddle(fiddleA.id, function (err, fiddleB) {
+          expect(fiddleA.id).equal(fiddleB.id);
+          rimraf(path.join(fiddleA.path, '..'), done);
+        });
+      });
+    });
+
+
+  });
+});
