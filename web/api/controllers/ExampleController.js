748928bef8ba7310e4914831db83315e59c987ee
Jason Stangroome <jason@squixa.com>
2014-11-04 10:20:22 +1100

    Add example two: remove information leaking response headers

--- web/api/controllers/ExampleController.js
+++ web/api/controllers/ExampleController.js
@@ -21,6 +21,21 @@ curl "http://www.vclfiddle.net/example/one"
       .header('Link', '<https://www.flickr.com/photos/crazymandi/8165527856/>; rel=alternate')
       .header('Cache-Control', 'public, max-age=0')
       .sendfile(path.join(__dirname, 'flickr-crazymandi-puppy.jpg'));
+  },
+
+  two: function (req, res) {
+/* vcl:
+vcl 4.0; backend default { .host = "www.vclfiddle.net"; .port = "80"; }
+
+sub vcl_deliver {
+  unset resp.http.Server;
+  unset resp.http.X-Powered-By;
+}
+*/
+/* curl:
+curl "http://www.vclfiddle.net/example/two"
+*/
+    return res.send('Where are the Server and X-Powered-By response headers?');
   }
 
 }
