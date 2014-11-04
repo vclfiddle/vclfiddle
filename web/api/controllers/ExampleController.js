3ea6666f5fb01ee2632ac1b54371f5b5c6d574c1
Jason Stangroome <jason@squixa.com>
2014-11-04 11:07:59 +1100

    Add example three: caching a non-personalised page in spite of session cookies

--- web/api/controllers/ExampleController.js
+++ web/api/controllers/ExampleController.js
@@ -36,6 +36,33 @@ sub vcl_deliver {
 curl "http://www.vclfiddle.net/example/two"
 */
     return res.send('Where are the Server and X-Powered-By response headers?');
+  },
+
+  three: function (req, res) {
+/* vcl:
+vcl 4.0; backend default { .host = "www.vclfiddle.net"; .port = "80"; }
+
+sub vcl_recv {
+  if (req.url == "/example/three") {
+    unset req.http.Cookie;
+  }
+}
+
+sub vcl_backend_response {
+  if (bereq.url == "/example/three") {
+    unset beresp.http.Set-Cookie;
+  }
+}
+*/
+/* curl:
+curl "http://www.vclfiddle.net/example/three" -H "Cookie: example-user-session=88c6cee414"
+curl "http://www.vclfiddle.net/example/three" -H "Cookie: example-user-session=358dfda63e"
+curl "http://www.vclfiddle.net/example/three" -H "Cookie: example-user-session=88c6cee414"
+*/
+    req.session = null;
+    return res
+      .header('Set-Cookie', 'example-user-session=f295e3b543; Path=/; HttpOnly')
+      .send('This page is not personalised and so a response for one user can safely be served for another even though it sets a session cookie.');
   }
 
 }
