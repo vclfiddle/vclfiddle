85a76f44c736a0e273a7f6e5af95b241d41484dc
csouza <csouza@outlook.com.au>
2015-02-03 13:28:02 +1100

    remove key

--- web/config/mandrill.js
+++ web/config/mandrill.js
@@ -1,6 +1,6 @@
 module.exports.mandrill = {
 
-	api_key: 'x6r6NSHIhD3ZMVwVQ2LESg',
+	api_key: '________________________',
 	prepend_subject: 'Contact Form | ',
 	from_email: 'info@vclfiddle.com',
 	from_name: 'VclFiddle'
