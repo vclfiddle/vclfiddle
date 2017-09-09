f41a843ba6f3241414e6a481c2b44bf3a1e13c63
Cleber J Santos <cleber@cleberjsantos.com.br>
2017-09-08 23:37:22 -0300

    Force tag image

--- buildimages.sh
+++ buildimages.sh
@@ -9,5 +9,5 @@ varnish2"
 
 while read -r image; do
     image_path="$(dirname $0)/${image}"
-    sudo docker build --force-rm --tag="${image}" $image_path
+    sudo docker build --force-rm --tag="${image}:latest" $image_path
 done <<< "$images"
