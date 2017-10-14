7e616313b74d15f785120fcd05e4089615eaed63
Cleber J Santos <cleber@cleberjsantos.com.br>
2017-10-14 11:07:26 -0300

    Add varnish 5.2.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,6 +34,7 @@ sudo rsync -av $SCRIPTROOT/web/ /var/web/
 pushd /var/web && sudo npm install && popd
 
 # build the Varnish docker image
+$SCRIPTROOT/varnish5_2_0/build.sh
 $SCRIPTROOT/varnish5_1_3/build.sh
 $SCRIPTROOT/varnish5_0_0/build.sh
 $SCRIPTROOT/varnish4_1_8/build.sh
